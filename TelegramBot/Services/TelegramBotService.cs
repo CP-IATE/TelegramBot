e3df703ea17d2b7e733d00260f62055c4b56b585
Daniil Mushtuk <mushtuk.d@gmail.com>
2025-03-28 23:06:31 +0200

    feat(botservice): removed file echoing feature

--- TelegramBot/Services/TelegramBotService.cs
+++ TelegramBot/Services/TelegramBotService.cs
@@ -39,74 +39,27 @@ public sealed class TelegramBotService : BackgroundService
             Console.WriteLine($"Message from {message.From.Id}\n {text}");
             if (message.Photo is { } photo)
             {
-                await HandleFileAsync(photo[0].FileId, "Files\\Photo", async (fileStream) =>
-                {
-                    await botClient.SendPhoto(
-                        message.Chat.Id,
-                        InputFile.FromStream(fileStream, photo[0].FileId + ".jpg"),
-                        caption: $"Message from {message.From.Id} with {photo[0].FileId}\n{text}",
-                        cancellationToken: cancellationToken
-                    );
-                }, cancellationToken);
+                await HandleFileAsync(photo[0].FileId, "Files\\Photo", cancellationToken);
             }
             else if (message.Document is { } document)
             {
-                await HandleFileAsync(document.FileId, "Files\\Documents", async (fileStream) =>
-                {
-                    await botClient.SendDocument(
-                        message.Chat.Id,
-                        InputFile.FromStream(fileStream, document.FileName),
-                        caption: $"Message from {message.From.Id} with {document.FileName}\n{text}",
-                        cancellationToken: cancellationToken
-                    );
-                }, cancellationToken);
+                await HandleFileAsync(document.FileId, "Files\\Documents", cancellationToken);
             }
             else if (message.Video is { } video)
             {
-                await HandleFileAsync(video.FileId, "Files\\Video", async (fileStream) =>
-                {
-                    await botClient.SendVideo(
-                        message.Chat.Id,
-                        InputFile.FromStream(fileStream, video.FileId + Path.GetExtension(video.FileName)),
-                        caption: $"Message from {message.From.Id} with {video.FileId}\n{text}",
-                        cancellationToken: cancellationToken
-                    );
-                }, cancellationToken);
+                await HandleFileAsync(video.FileId, "Files\\Video", cancellationToken);
             }
             else if (message.Audio is { } audio)
             {
-                await HandleFileAsync(audio.FileId, "Files\\Audio", async (fileStream) =>
-                {
-                    await botClient.SendAudio(
-                        message.Chat.Id,
-                        InputFile.FromStream(fileStream, audio.FileId + Path.GetExtension(audio.FileName)),
-                        caption: $"Message from {message.From.Id} with {audio.FileId}\n{text}",
-                        cancellationToken: cancellationToken
-                    );
-                }, cancellationToken);
+                await HandleFileAsync(audio.FileId, "Files\\Audio", cancellationToken);
             }
             else if (message.Voice is { } voice)
             {
-                await HandleFileAsync(voice.FileId, "Files\\Voice", async (fileStream) =>
-                {
-                    await botClient.SendVoice(
-                        message.Chat.Id,
-                        InputFile.FromStream(fileStream, voice.FileId + ".ogg"),
-                        caption: $"Message from {message.From.Id} with {voice.FileId}\n{text}",
-                        cancellationToken: cancellationToken
-                    );
-                }, cancellationToken);
+                await HandleFileAsync(voice.FileId, "Files\\Voice", cancellationToken);
             }
             else if (message.Sticker is { } sticker)
             {
-                await HandleFileAsync(sticker.FileId, "Files\\Sticker", async (fileStream) =>
-                {
-                    await botClient.SendSticker(
-                        message.Chat.Id,
-                        InputFile.FromFileId(sticker.FileId),
-                        cancellationToken: cancellationToken
-                    );
-                }, cancellationToken);
+                await HandleFileAsync(sticker.FileId, "Files\\Sticker", cancellationToken);
             }
             else
             {
@@ -129,7 +82,7 @@ public sealed class TelegramBotService : BackgroundService
         return Task.CompletedTask;
     }
     
-    private async Task HandleFileAsync(string fileId, string directoryPath, Func<FileStream, Task> sendFileFunc, CancellationToken cancellationToken)
+    private async Task HandleFileAsync(string fileId, string directoryPath, CancellationToken cancellationToken)
     {
         var file = await _botClient.GetFile(fileId, cancellationToken);
         var filePath = Path.Combine(directoryPath, fileId + Path.GetExtension(file.FilePath));
@@ -138,11 +91,6 @@ public sealed class TelegramBotService : BackgroundService
         await using (var fileStream = new FileStream(filePath, FileMode.Create))
         {
             await _botClient.DownloadFile(file.FilePath, fileStream, cancellationToken);
-        }
-
-        await using (var fileStream = new FileStream(filePath, FileMode.Open))
-        {
-            await sendFileFunc(fileStream);
         }
     }
 }
