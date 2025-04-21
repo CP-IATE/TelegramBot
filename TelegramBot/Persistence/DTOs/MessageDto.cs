ee78acdec2db20c9ce745c4fff8a7fc39e1e62dc
Daniil Mushtuk <mushtuk.d@gmail.com>
2025-04-21 18:48:08 +0300

    fix(messageDto): fix dto to create empty list in SendMessageRequest

--- TelegramBot/Persistence/DTOs/MessageDto.cs
+++ TelegramBot/Persistence/DTOs/MessageDto.cs
@@ -5,5 +5,5 @@ namespace TelegramBot.Persistence.DTOs;
 public class MessageDto
 {
     public string? text { get; set; }
-    public List<AttachmentDto>? attachments { get; set; }
+    public List<AttachmentDto> attachments { get; set; } = [];
 }
